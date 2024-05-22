cb6476e6be552fcb6057c9dbcdde5c929439ae96
irene <irenemoraleess22@gmail.com>
2024-05-22 22:01:24 +0200

    Variables de estado creadas

--- mi-app/src/components/App.jsx
+++ mi-app/src/components/App.jsx
@@ -1,7 +1,9 @@
 
 function App() {
   const [foods, setFoods] = useState ([]);
-  const [name, setName] = useState ()
+  const [name, setName] = useState ("");
+  const [type, setType] = useState ([]);
+  const [day, setDay] = useState ("");
   return (
     <div>App</div>
   )
